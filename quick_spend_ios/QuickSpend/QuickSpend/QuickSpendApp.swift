a35c16032ea3828649e71b332c75b9e7fc5ec253
TaiPV <taipv@stringee.com>
2026-02-12 08:40:26 +0700

    create new swiftui native project

---
+++ quick_spend_ios/QuickSpend/QuickSpend/QuickSpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSpendApp.swift
+//  QuickSpend
+//
+//  Created by TaiPV on 10/2/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickSpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
